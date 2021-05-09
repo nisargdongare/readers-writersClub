96266f8130ae7644b15d0416514c6872b29a1461
nisargdongare <drnfriend@gmail.com>
2021-05-09 15:16:32 +0530

    asad

--- backend_rwclub/server/routes/deviceRouter.js
+++ backend_rwclub/server/routes/deviceRouter.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const deviceRouter = express.Router();
-const jwt = "1aBjAEo0QgqCWZDwIwDiMmLyGQxFos";
+const SecretKey = "1aBjAEo0QgqCWZDwIwDiMmLyGQxFos";
 const deviceDB = require('../db/deviceDB');
 
 
 //1)create Device
 deviceRouter.post('/', async (req, resp, next) => {
     try {
-        let data = {
-            studentId: req.body.studentId,
-            deviceId: req.body.deviceId
-        }
-        let results = await deviceDB.createDevice(data);
-        resp.send(results);
+        console.log(req.query);
+        if(req.query.secret_token === SecretKey){
+            let data = {
+                studentId: req.body.studentId,
+                deviceId: req.body.deviceId
+            }
+            let results = await deviceDB.createDevice(data);
+            resp.send(results);
+        }else {resp.json([]);}
     } catch (err) {
         console.log(err);
         resp.status(500).json(err.sqlMessage);
@@ -23,12 +26,14 @@ deviceRouter.post('/', async (req, resp, next) => {
 //2)update Device by DeviceNumber
 deviceRouter.put('/', async (req, resp, next) => {
     try {
-        let data = {
-            studentId: req.body.studentId,
-            deviceId: req.body.deviceId
-        }
-        let results = await deviceDB.updateDevice(data);
-        resp.send(results);
+        if(req.query.secret_token === SecretKey){
+            let data = {
+                studentId: req.body.studentId,
+                deviceId: req.body.deviceId
+            }
+            let results = await deviceDB.updateDevice(data);
+            resp.send(results);
+        }else {resp.json([]);}
     } catch (err) {
         console.log(err);
         resp.status(500).json(err.sqlMessage);
@@ -38,11 +43,11 @@ deviceRouter.put('/', async (req, resp, next) => {
 //3)remove Device by DeviceNumber
 deviceRouter.delete('/', async (req, resp, next) => {
     try {
-        let data = {
-            studentId: req.body.studentId
-        }
-        let results = await deviceDB.deleteDevice(data);
-        resp.json(results);
+        let data = {studentId: req.body.studentId}
+        if(req.query.secret_token === SecretKey){
+            let results = await deviceDB.deleteDevice(data);
+            resp.json(results);
+        }else {resp.json([]);}
     } catch (err) {
         console.log(err);
         resp.status(500).json(err.sqlMessage);
@@ -53,8 +58,10 @@ deviceRouter.delete('/', async (req, resp, next) => {
 //4)get Device details & send OTP
 deviceRouter.get('/:studentId', async (req, resp, next) => {
     try {
-        let results = await deviceDB.searchDevice(req.params.studentId);
-        resp.json(results);
+        if(req.query.secret_token === SecretKey){
+            let results = await deviceDB.searchDevice(req.params.studentId);
+            resp.json(results);
+        }else {resp.json([]);}
     } catch (err) {
         console.log(err);
         resp.status(500).json(err.sqlMessage);
--- rwclub/src/App.js
+++ rwclub/src/App.js
@@ -10,26 +10,90 @@ const App = () => {
   const [searchPopup, setSearchPopup] = useState(false);
   const [studentState, setStudentState] = useState(0);
   const [studentId, setStudentId] = useState("");
-  const [deviceId, setDeviceId] = useState("B298WrD");
+  const [studentName, setStudentName] = useState("---------");
+  const [deviceId, setDeviceId] = useState("");
   const [message, setMessage] = useState("");
+  const [action, setAction] = useState(0);
   const url = "http://localhost:3000/api/device/";
+
+  const addDevice = async () => {
+    if (deviceId === "") { alert("Kindly enter valid \"device\" Id."); return; }
+    let resp;
+    let data = {
+      studentId: studentId,
+      deviceId: deviceId
+    }
+    await axios.post(url, data, { params: { secret_token: '1aBjAEo0QgqCWZDwIwDiMmLyGQxFos' } })
+      .then(response => { resp = response.data; })
+      .catch((error) => { alert(error); });
+
+    let count = Object.keys(resp).length;
+    if (count) { alert("Device Saved Successful"); }
+    else { alert("Device Save Failed"); }
+    clearMemory();
+  }
+
+  const clearMemory = () => { setStudentId(""); setDeviceId(""); setSearchPopup(false); setStudentState(0); setMessage(""); setAction(0); }
+
+  const editDevice = async () => {
+    if (deviceId === "") { alert("Kindly enter valid \"device\" Id."); return; }
+    let resp;
+    let data = {
+      studentId: studentId,
+      deviceId: deviceId
+    }
+    await axios.put(url, data, { params: { secret_token: '1aBjAEo0QgqCWZDwIwDiMmLyGQxFos' } })
+      .then(response => { resp = response.data; })
+      .catch((error) => { alert(error); });
+
+    let count = Object.keys(resp).length;
+    if (count) { alert("Device Updated Successful"); }
+    else { alert("Device Save Failed"); }
+    clearMemory();
+  }
+
+  const deleteDevice = async () => {
+    let resp;
+    let data = { studentId: studentId }
+    await axios.delete(url + "?secret_token=1aBjAEo0QgqCWZDwIwDiMmLyGQxFos", { data: data })
+      .then(response => { resp = response.data; })
+      .catch((error) => { alert(error); });
+    let count = Object.keys(resp).length;
+    if (count) { alert("Student - Device record deleted successfully."); }
+    else { alert("Device delete Failed"); }
+    clearMemory();
+  }
 
   const getDeviceSearch = async () => {
     if (studentId === "") { alert("Kindly enter valid \"student\" Id to search"); return; }
     let resp;
-    await axios.get(url + studentId)
+    var bodyFormData = new FormData();
+    bodyFormData.append('sid', studentId);
+    bodyFormData.append('secret_token', '1aBjAEo0QgqCWZDwIwDiMmLyGQxFos');
+
+    // axios({method: "post", url: 'https://corewords.in/apis/getStudent.php', data: bodyFormData, headers: { "Content-Type": "multipart/form-data" },})
+    //   .then(response => {
+    //     console.log(response.data);
+    //     resp = response.data;
+    //   }).catch((error) => {
+    //     alert(error);
+    //   });
+    setStudentName("----------------");
+    await axios.get(url + studentId, { params: { secret_token: '1aBjAEo0QgqCWZDwIwDiMmLyGQxFos' } })
       .then(response => {
-        console.log(response.data);
         resp = response.data;
       }).catch((error) => {
         alert(error);
       });
-    // ???????? till yet
-
-    let student = resp[0].studentId;
-    if (student !== undefined) { console.log(student) }
-    else { console.log("it failed") }
-    // setStudentState(2); setMessage("Student Id is new. Kindly Enter Device Id to create one.");
+    if (resp.length) {
+      setDeviceId(resp[0].deviceId);
+      setStudentState(2);
+      setMessage("Student Id is available in the database.");
+    }
+    else {
+      setStudentState(1);
+      setMessage("Student Id is new. Kindly Enter Device Id to create one.");
+    }
   }
   return (
     <div>
@@ -46,16 +110,16 @@ const App = () => {
         <LeftBox>
           <TextTitle>Readers & Writers </TextTitle>
           <TextTitle style={{ marginBottom: '5%' }} >Club </TextTitle>
-          <text style={{ fontSize: '30px', fontWeight: 'bold', marginBottom: '2%' }} >Todays Reader is tomorrow's Writer</text>
-          <text style={{ fontSize: '25px', marginBottom: '5%', justifySelf: 'center' }} >Reading and writing are among the most critical skill cultivated in schools.
+          <Text style={{ fontSize: '30px', fontWeight: 'bold', marginBottom: '2%' }} >Todays Reader is tomorrow's Writer</Text>
+          <Text style={{ fontSize: '25px', marginBottom: '5%', justifySelf: 'center' }} >Reading and writing are among the most critical skill cultivated in schools.
           However, there isn’t enough emphasis on analysing the information they read and transferring the same processed information through thoughtful writing.
           This course intends to introduce children to the concept of critical thinking while providing them strategies to help them think, read, take notes and
-          write critically. </text>
+          write critically. </Text>
           <Button variant="contained" autoCapitalize="none" onClick={() => window.open("https://readersandwritersclub.in/")} style={{ textTransform: 'none', borderRadius: '2rem', fontSize: '20px', backgroundColor: '#bfa060', color: 'white', borderWidth: '5px', }}>Learn More</Button>
         </LeftBox>
         <RightBox>  <img style={{ margin: '1%', width: '80%' }} src={readerImage} alt="Logo" /> </RightBox>
       </Container>
-      <Modal open={searchPopup} onClose={() => { setSearchPopup(false); setStudentState(0); setMessage(""); }}>
+      <Modal open={searchPopup} onClose={clearMemory}>
         <PopupContainer>
           <TextKey style={{ color: '#bfa060', fontSize: '2.5rem' }}>Student-Device Dashboard</TextKey>
           <TextValue style={{ color: 'green' }}>{message}</TextValue>
@@ -67,19 +131,19 @@ const App = () => {
           </div> : []}
           {studentState !== 0 ? <div>
             <TextKey>Student Name :</TextKey>
-            <TextValue>Jija Nisarg Dongare</TextValue>
+            <TextValue>{studentName}</TextValue>
           </div> : []}
           {studentState === 2 ? <div>
             <TextKey>Student already Registered with Device :</TextKey>
             <TextValue style={{ color: 'red' }}>{deviceId}</TextValue>
           </div> : []}
           {studentState === 2 ? <div style={{ position: 'relative', paddingLeft: '60%' }}>
-            <Button variant="contained" onClick={() => { setSearchPopup(false); setStudentState(0); setMessage(""); }} startIcon={<IconDelete />} style={{ marginLeft: '1rem', fontSize: '20px', backgroundColor: 'red', color: 'black', textTransform: 'none' }}>Delete Setting</Button>
-            <Button variant="contained" onClick={() => { setStudentState(1); setMessage("Kindly Enter New Device Id :") }} startIcon={<IconEdit />} style={{ marginLeft: '1rem', fontSize: '20px', backgroundColor: 'grey', color: 'black', textTransform: 'none' }}>Change Device</Button>
+            <Button variant="contained" onClick={deleteDevice} startIcon={<IconDelete />} style={{ marginLeft: '1rem', fontSize: '20px', backgroundColor: 'red', color: 'black', textTransform: 'none' }}>Delete Setting</Button>
+            <Button variant="contained" onClick={() => { setAction(1); setStudentState(1); setMessage("Kindly Enter New Device Id :") }} startIcon={<IconEdit />} style={{ marginLeft: '1rem', fontSize: '20px', backgroundColor: 'grey', color: 'black', textTransform: 'none' }}>Change Device</Button>
             <Button variant="contained" onClick={() => { setSearchPopup(false); setStudentState(0); setMessage("") }} style={{ marginLeft: '1rem', fontSize: '20px', backgroundColor: 'green', color: 'black', textTransform: 'none' }}>Close</Button>
           </div> : []}
           {studentState === 1 ? <TextField id="outlined-basic" onChange={(txt) => { setDeviceId(txt.target.value) }} label="Enter Device Id ...." variant="outlined" style={{ marginTop: '1%' }} /> : []}
-          {studentState === 1 ? <Button variant="contained" onClick={() => { setSearchPopup(false); setStudentState(0); setMessage(""); }} startIcon={<IconSave />} style={{ borderRadius: '5rem', width: ' 20%', marginTop: '1%', fontSize: '20px', backgroundColor: 'green', color: 'white', textTransform: 'none' }}>Save New Settings</Button> : []}
+          {studentState === 1 ? <Button variant="contained" onClick={() => { action ? editDevice() : addDevice() }} startIcon={<IconSave />} style={{ borderRadius: '5rem', width: ' 20%', marginTop: '1%', fontSize: '20px', backgroundColor: 'green', color: 'white', textTransform: 'none' }}>Save New Settings</Button> : []}
         </PopupContainer>
       </Modal>
     </div>
@@ -93,5 +157,6 @@ const PopupContainer = styled.div`margin : 5%; height: 60%; background-color: wh
 const TextTitle = styled.text`font-size: 60px; font-weight: bold; color: #bfa060 `;
 const TextKey = styled.text`margin-top: 2%; font-size: 1.5rem; padding: 10px; color: black; font-weight: bolder`;
 const TextValue = styled.text`font-size: 1.5rem; padding: 10px; color: grey; font-weight: bolder;`;
+const Text = styled.text``;
 
 export default App;
